25af82f310cca863ebe92cd15c576986f291cb5f
BernhardBaumrock <office@baumrock.com>
2024-10-22 14:01:32 +0200

    feat: add getData() method for StepsArray

--- classes/StepsArray.php
+++ classes/StepsArray.php
@@ -3,7 +3,21 @@
 namespace RockForms;
 
 use ProcessWire\WireArray;
+use ProcessWire\WireData;
 
 class StepsArray extends WireArray
 {
+  public function getData(
+    $stepname = null,
+    $returnArrays = false,
+  ): array|WireData {
+    $data = [];
+    foreach ($this as $step) {
+      $data[$step->name] = $returnArrays
+        ? $step->getData()->getArray()
+        : $step->getData();
+    }
+    if ($stepname) return $data[$stepname];
+    return $data;
+  }
 }
